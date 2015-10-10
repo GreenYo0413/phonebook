de1f0e217f867f0ab530f04da4c7c8fcd5af124b
GreenYo0413 <f0246810@gmail.com>
2015-10-10 14:43:40 +0800

    original version

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -3,20 +3,23 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
-/* original version */
+
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
+    dataLink* pdata;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
+
+typedef struct __DataLink{
+	char firstName[16]
+	char email[16];
+	char phone[10];
+	char cell[10];
+	char addr1[16];
+	char addr2[16];
+	char city[16];
+	char state[2];
+}dataLink;
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
